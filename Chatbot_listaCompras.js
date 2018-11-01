46abd337f0fa2fc66c9f090e125fbf6b283e11bd
pedroalpacheco <pedro.pacheco.a@gmail.com>
2018-11-01 15:02:46 -0300

    Modificado start

--- Chatbot_listaCompras.js
+++ Chatbot_listaCompras.js
@@ -17,7 +17,7 @@ bot.use(session());
 bot.start(async ctx => {
     const name = ctx.update.message.from.first_name
     await ctx.reply(`Seja bem vindo, ${name}!`)
-    await ctx.reply(`Escreva os itens que você deseja adicionar...`)
+    await ctx.reply(`Escreva sua lista de compras...`)
     ctx.session.lista = []
 })
 
