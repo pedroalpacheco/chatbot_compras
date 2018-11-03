b714ae4b5ecd1edf5dd5dbc22a3803f1ea7e8503
pedro.pacheco.a@gmail.com <pedro.pacheco.a@gmail.com>
2018-11-02 22:09:42 -0300

    Por sessão

--- Chatbot_listaCompras.js
+++ Chatbot_listaCompras.js
@@ -1,35 +1,38 @@
-const env = require('./.env');
-const Telegraf = require('telegraf');
-const Extra = require('telegraf/extra');
-const Markup = require('telegraf/markup');
-const session = require('telegraf/session');
+const env = require('./.env')
+const Telegraf = require('telegraf')
+const Extra = require('telegraf/extra')
+const Markup = require('telegraf/markup')
+const session = require('telegraf/session')
 const bot = new Telegraf(env.token)
 
 const botoes = lista => Extra.markup(
     Markup.inlineKeyboard(
         lista.map(item => Markup.callbackButton(item, `delete ${item}`))
-        ,{columms:3}
+            , {columns: 3}
     )
 )
 
-bot.use(session());
+bot.use(session())
 
 bot.start(async ctx => {
     const name = ctx.update.message.from.first_name
     await ctx.reply(`Seja bem vindo, ${name}!`)
-    await ctx.reply(`Escreva sua lista de compras...`)
+    await ctx.reply('Escreva os itens de sua lista de compras!.')
     ctx.session.lista = []
 })
 
-bot.on('text', ctx =>{
+bot.on('text', ctx => {
     let msg = ctx.update.message.text
     ctx.session.lista.push(msg)
-    ctx.reply(`${msg} adicionado!`,botoes(ctx.session.lista))
+    ctx.reply(`${msg} adicionado!`, botoes(ctx.session.lista))
+    console.log(`${msg} adicionado!`)
 })
 
-bot.action(/delete (.+)/,ctx =>{
+bot.action(/delete (.+)/, ctx => {
     ctx.session.lista = ctx.session.lista.filter(
         item => item !== ctx.match[1])
     ctx.reply(`${ctx.match[1]} deletado!`, botoes(ctx.session.lista))
+    console.log(`${ctx.match[1]} deletado!`)
 })
+
 bot.startPolling()
